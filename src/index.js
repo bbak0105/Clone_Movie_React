8f3c2b5c3a1c9aa6c2aab3d730771acfbfff78ba
jiyuShin <jiyuShin@douzone.com>
2022-02-07 17:59:51 +0900

    160p까지 commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// App 컴포넌트를 ReactDOM에 id가 root인 엘리먼트에 그린다고 생각하면 된다.
+// 리액트는 우리가 작성한 프로젝트 폴더에 있는 코드를 자바스크립트를 이용하여 해석한다.
+// 그리고 해석한 결과물을 index.html로 끼워 넣는다.
+
